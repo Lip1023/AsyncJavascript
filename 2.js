6fd1950133091cb845956791f5b4dda7230934cd
Pullip Lee <pullip.lee1023@gmail.com>
2020-10-13 19:54:43 +0800

    async2

--- 2.js
+++ 2.js
@@ -1,12 +1,27 @@
+const getTodos = (callback) => {
 // XML is all the types of data 
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if(request.readyState===4){
-        console.log(request.responseText)
+    // request is complete and successful
+    if(request.readyState===4 && request.status ===200){
+        callback(undefined, request.responseText);
+    }
+    // request is complete but data request is not successful
+    else if(request.readyState=== 4){
+        callback('could not fetch data', undefined);
     }
 })
 
-request.open('GET', 'http://jsonplaceholder.typicode.com/todos/');
+request.open('GET', 'http://jsonplaceholder.typicode.com/todoss/');
 request.send();
+};
+// convention wise err always comes first
+getTodos((err, data)=>{
+if(err){
+console.log(err);}
+else{
+console.log(data)
+}
+});
+
